3f090120be1d3a45dbd98c75deb25a68b7247b11
Zeva-yy <yeyong.yyt@gmail.com>
2020-12-11 01:39:51 +0800

    geolocation

---
+++ week05/geolocation.js
@@ -0,0 +1,14 @@
+let elLocate = document.getElementById("locate");
+elLocate.addEventListener("click", function(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            let elLat = document.getElementById("lat");
+            let ellong = document.getElementById("long");
+
+            elLat.innerHTML = "Latitude : " + position.coords.latitude;
+            ellong.innerHTML = "Longtitude : " + position.coords.longitude;
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.")
+    }
+});

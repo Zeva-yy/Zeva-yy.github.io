4f7f1f90d5a6ffc5853efd4556e3c87af8a72034
Zeva-yy <yeyong.yyt@gmail.com>
2020-12-22 20:51:44 +0800

    2051

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -27,5 +27,19 @@ function GetBookings() {
 
                 bookingIds.push(buttonId);
             }
+
+            for (let j = 0; j < bookingIds.length; j++) {
+                let el = document.getElementById(bookingIds[j]);
+                el.addEventListener("click", function () {
+                    let theId = bookingIds[j].replace("delete", "");
+                    alert(theId);
+                });
+            }
         });
 }
+
+window.addEventListener("load", function () {
+    document.getElementById("refreshList").addEventListener("click", function () {
+        GetBookings()
+    });
+});

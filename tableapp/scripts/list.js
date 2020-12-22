eb516cc5635c2a067fd0b57a475f02431cd4f7e2
Zeva-yy <yeyong.yyt@gmail.com>
2020-12-22 21:29:36 +0800

    2129

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -9,6 +9,11 @@ function GetBookings() {
             let bookingList = document.getElementById("booking-list");
             let bookingIds = []; //to choose which row to delete
 
+            //clear the table rows
+            for (let k = bookingList.rows.length - 1; k > 0; k--) {
+                bookingList.deleteRow(k);
+            }
+
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
